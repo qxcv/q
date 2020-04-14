29f9c19860d2be3333363c1bc13f59bfdbd15dbc
Sam Toyer <qxcv@users.noreply.github.com>
2020-04-14 16:49:53 -0700

    use .__version__ instead of __version__

--- bin/q.py
+++ bin/q.py
@@ -30,7 +30,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __version__ import q_version
+from .__version__ import q_version
 
 __all__ = [ 'QTextAsData' ]
 
